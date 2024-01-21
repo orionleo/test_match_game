b3c2b8b804e762055d3123bb02c5a6dcd962417c
Jai Aggarwal <jaiaggarwal60@gmail.com>
2024-01-21 22:07:33 +0530

    made css changes

--- components/Card.tsx
+++ components/Card.tsx
@@ -16,13 +16,11 @@ interface CardProps {
 //is carried out in this file 
 function Card({ item, handleSelectedCards, toggled, stopFlip }: CardProps) {
 	return (
-		<div className="w-1/2 h-1/2">
-			<div className="w-full h-full">
+		<div >
+			<div>
 				<div>
 				<img
-					// width={200}
-					// height={200}
-					className={`h-[6rem] w-[6rem] absolute [transform:rotateY(90deg)] transition-all rounded-[50%] ease-in duration-[250ms] ${toggled && "[transform:rotateY(-1deg)] delay-[250ms]"}`} src={item.img} alt="face" />
+					className={`max-h-[6rem] max-w-[6rem] min-h-[3rem] min-w-[3rem] absolute [transform:rotateY(90deg)] transition-all rounded-[50%] ease-in duration-[250ms] ${toggled && "[transform:rotateY(-0deg)] delay-[250ms]"}`} src={item.img} alt="face" />
 				</div>
 				<div
 					className={`h-[6rem] w-[6rem] bg-[#fddaaf] text-[rgb(61, 21, 21)] border-[1px] border-solid border-black rounded-[50%] transition-all ease-in duration-[250ms] delay-[250ms] ${toggled && "[transform:rotateY(90deg)] delay-0"} `}
