999aad59711a48c5f4230c84fc3d35c987de6468
Jai Aggarwal <jaiaggarwal60@gmail.com>
2024-01-21 22:48:09 +0530

    more changes for the image

--- components/Card.tsx
+++ components/Card.tsx
@@ -18,10 +18,13 @@ function Card({ item, handleSelectedCards, toggled, stopFlip }: CardProps) {
 	return (
 		<div className="h-full w-full" >
 			<div className="h-full w-full">
-				<div className="h-full w-full">
+				{toggled ? (
 					<img
-						className={`h-[6rem] absolute [transform:rotateY(90deg)] transition-all rounded-[50%] ease-in duration-[250ms] ${toggled && "[transform:rotateY(0deg)] transition-all rounded-[50%] ease-in duration-[250ms] delay-[250ms]"}`} src={item.img} alt="face" />
-				</div>
+						className={`h-[6rem] [transform:rotateY(0deg)] absolute transition-all rounded-[50%] ease-in delay-[250ms]`} src={item.img} alt="face" />
+				) : (<img
+					className={`h-[6rem] [transform:rotateY(90deg)] absolute transition-all rounded-[50%] ease-in duration-[250ms]`} src={item.img} alt="face" />)}
+
+
 				<div
 					className={`h-[6rem] w-[6rem] bg-[#fddaaf] text-[rgb(61, 21, 21)] border-[1px] border-solid border-black rounded-[50%] transition-all ease-in duration-[250ms] delay-[250ms] ${toggled && "[transform:rotateY(90deg)] delay-0"} `}
 					onClick={() => !stopFlip && handleSelectedCards(item)}
